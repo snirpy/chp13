0a519ad7d43ce7b561d3be121d3e2ff8e47aaa12
snirpy <76662720+snirpy@users.noreply.github.com>
2023-11-24 14:53:54 +0100

    Update codeDepartPassword.py

--- codeDepartPassword.py
+++ codeDepartPassword.py
@@ -3,7 +3,9 @@ import re
 passwords = "P@ssw0rd, strongP@ss, 12345678, WeakPassword, !/_y@$K0rr1"
 
 # Votre expression régulière ici
-pattern = re.compile(r'')  # À compléter
+# pattern = re.compile(r'')  # À compléter
+pattern = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*/_]).{8,}$')
+
 
 # Utilisez la méthode findall pour obtenir tous les mots de passe valides
 valid_passwords = pattern.findall(passwords)
